f79e6146b3ee5a83ecf372682a77b0932ec3ca63
NaokiSawamura <n.sawamura48@gmail.com>
2019-10-16 17:47:55 +0900

    create tweet main view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_id, :name])
+  def show
+    @user = User.find(params[:id])
   end
   def new
   end
